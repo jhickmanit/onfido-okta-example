438b62aca97a5ad85a4faceb1d09fa8393685078
Jeff Hickman <jhickman.it@gmail.com>
2021-02-25 08:40:37 -0800

    updated cors config

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const sess = {
 };
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'http://localhost:3001'],
   optionsSuccessStatus: 200,
   credentials: true,
 };
